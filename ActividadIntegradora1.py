a6749d53f4f03d90511fd8b6bcb5a9ce2bbfd446
samtrrs52 <98476206+samtrrs52@users.noreply.github.com>
2023-06-05 19:09:35 -0600

    Update3 ActividadIntegradora1.py

--- ActividadIntegradora1.py
+++ ActividadIntegradora1.py
@@ -16,6 +16,6 @@ df = pd.read_csv('https://drive.google.com/file/d/1sUXMxryvguQp81yseLxYymTX5RxaH
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
-mapa=pd.DataFrame()
+mapa=df[['Latitude', 'Longitude']]
 mapa=mapa.dropna()
 st.map(mapa.astype(int))
