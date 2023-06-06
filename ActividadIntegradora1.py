8a25f56b2fb0bbec97e3426ddcb40f6e0cd4533c
samtrrs52 <98476206+samtrrs52@users.noreply.github.com>
2023-06-05 19:10:29 -0600

    Update4 ActividadIntegradora1.py

--- ActividadIntegradora1.py
+++ ActividadIntegradora1.py
@@ -13,7 +13,7 @@ import pandas as pd
 st.title('POLICE INCIDENT REPORTS FROM 2018 TO 2020 IN SAN FRANCISCO')
 
 df = pd.read_csv('https://drive.google.com/file/d/1sUXMxryvguQp81yseLxYymTX5RxaH0e9/view?usp=drive_link/Police_Department_Incident_Reports__2018_to_Present.csv')
-
+df.describe()
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
 mapa=df[['Latitude', 'Longitude']]
