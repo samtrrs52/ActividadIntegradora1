abfd978e4f3ee5934dc2476c7bcd3e6a7809ef99
samtrrs52 <98476206+samtrrs52@users.noreply.github.com>
2023-06-06 19:56:02 -0600

    Update actividadintegradora1.py

--- actividadintegradora1.py
+++ actividadintegradora1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('https://github.com/samtrrs52/ActividadIntegradora1/blob/bed49a7bf39fc0e5b03a5e6c3f991a5db97670e9/Prueba_Police_Department_Incident_Reports__2018_to_Present.csv')
+df = pd.read_csv('https://github.com/samtrrs52/ActividadIntegradora1/blob/bed49a7bf39fc0e5b03a5e6c3f991a5db97670e9/Prueba_Police_Department_Incident_Reports__2018_to_Present.csv', low_memory=False)
 
 st.dataframe(df)
 
